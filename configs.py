1fa2de1a3c168d673c5b060f9dd8fb2d9e309eda
xsboy-12345 <xsboy12345@gmail.com>
2025-04-02 15:58:56 +0800

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,10 +1,16 @@
-# configs.py
+import torch
 
 batch_size = 128
 epochs = 20
 learning_rate = 0.001
 num_classes = 10
+
+# 数据路径
 cinic_path = './CINIC-10'
 cifar_path = './data'
+
+# 模型保存路径
 model_path = './checkpoints'
-device = 'cuda'  # or 'cpu'
+
+# 自动根据是否有GPU选择设备
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
